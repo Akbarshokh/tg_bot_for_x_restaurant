8e18a72cbf2250c2a0e5c21e2a1dd1e96bde75f8
Akhrorbek <ibrokhimovakhrorbek20@gmail.com>
2024-12-04 15:46:52 +0500

    changed into one style

--- loader.py
+++ loader.py
@@ -2,9 +2,11 @@
 # from aiogram.client.default import DefaultBotProperties
 # from aiogram.enums import ParseMode
 from aiogram import Bot, Dispatcher
+from aiogram.client.session.aiohttp import AiohttpSession
 
-from utils.config import BOT_TOKEN
+from utils.config import BOT_TOKEN, PROXY_CONFIG
+session = AiohttpSession(**PROXY_CONFIG) if PROXY_CONFIG["proxy"] else None
+bot = Bot(token=BOT_TOKEN, session=session)
 
-
-bot = Bot(token=BOT_TOKEN)
+# bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
